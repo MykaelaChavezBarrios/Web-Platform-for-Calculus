ccd88fab3a887124f97cc3d3e0826708ec258571
MykaelaChavezBarrios <tatyana.chavez@ucsm.edu.pe>
2024-11-05 04:30:00 -0500

    fix: routes under same name

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { loginAuth } from '../controllers/authC.js';
+import { logout } from '../controllers/authC.js';
 const router = Router();
 
 // Ruta para mostrar el formulario de inicio de sesión
@@ -10,7 +11,31 @@ router.get('/', (req, res) => {
 // Ruta para procesar el inicio de sesión
 router.post('/loginAuth', loginAuth);
 
+router.get('/logout', logout);
+
 // Rutas para los dashboards
+
+router.get('/inicio', (req, res) => {
+    // Verificar si el usuario tiene una sesión activa
+    if (!req.session.user) {
+        return res.redirect('/login');
+    }
+
+    // Redirige al dashboard correspondiente según el rol
+    if (req.session.user && req.session.user.rol === 'estudiante') {
+        res.render('home-E', { layout: 'main-E', title: 'Inicio' });
+    }
+
+    else if (req.session.user && req.session.user.rol === 'docente') {
+        res.render('home-P', { layout: 'main-P', title: 'Inicio' });
+    }
+
+    else {
+        res.redirect('/'); // Redirige al login si no es docente
+    }
+});
+
+/*
 router.get('/inicio-docente', (req, res) => {
     if (req.session.user && req.session.user.rol === 'docente') {
         res.render('home-P', { layout: 'main-P', title: 'Inicio' });
@@ -25,32 +50,47 @@ router.get('/inicio', (req, res) => {
     } else {
         res.redirect('/'); // Redirige al login si no es estudiante
     }
-});
+});*/
 
 // Rutas de acceso a Mi Perfil
 
 router.get('/mi-perfil', (req, res) => {
-    res.render('profile', { layout: 'main-E', title: 'Mi perfil' });
+    // Verificar si el usuario tiene una sesión activa
+    if (!req.session.user) {
+        return res.redirect('/login');
+    }
+
+    // Redirige al perfil correspondiente según el rol
+    if (req.session.user.rol === 'estudiante') {
+        res.render('profile', { layout: 'main-E', title: 'Mi perfil' });
+    } else if (req.session.user.rol === 'docente') {
+        res.render('profile', { layout: 'main-P', title: 'Mi perfil' });
+    }
 });
 
-router.get('/mi-perfil-docente', (req, res) => {
-    res.render('profile', { layout: 'main-P', title: 'Mi perfil' });
-});
 
 // Rutas de acceso a Cambiar constraseña
 
 router.get('/mi-perfil/cambiar-contrasena', (req, res) => {
-    res.render('password', { layout: 'main-E', title: 'Cambiar Contraseña' });
+    // Verifica si el usuario tiene una sesión activa
+    if (!req.session.user) {
+        return res.redirect('/login');
+    }
+
+    // Redirige al usuario según su rol
+    if (req.session.user.rol === 'estudiante') {
+        res.render('password', { layout: 'main-E', title: 'Cambiar Contraseña' });
+    } else if (req.session.user.rol === 'docente') {
+        res.render('password', { layout: 'main-P', title: 'Cambiar Contraseña' });
+    }
 });
 
-router.get('/mi-perfil-docente/cambiar-contrasena-docente', (req, res) => {
-    res.render('password', { layout: 'main-P', title: 'Cambiar Contraseña' });
-});
 
-export default router;
 
 // Ruta de contenido
 
 router.get('/contenido', (req, res) => {
     res.render('content', { layout: 'main-E', title: 'Contenido' });
 });
+
+export default router;
