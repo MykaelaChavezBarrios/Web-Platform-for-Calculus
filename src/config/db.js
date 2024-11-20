28ec49bda0ef3c14313214c7c90733466d90860c
MykaelaChavezBarrios <tatyana.chavez@ucsm.edu.pe>
2024-11-20 07:51:34 -0500

    feat: env for database variables

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,15 @@
 import { createPool } from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+// Cargar el archivo .env desde la carpeta /src
+dotenv.config({ path: 'src/.env' });
 
 const pool = createPool({
-    host: 'localhost',
-    port: '3306',
-    user: 'root',
-    password: 'clavenueva',
-    database: 'calcudb'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 export default pool;
